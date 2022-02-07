dc220b84acd6645b61abc82712955ed6a4e7662e
phj1120 <hyeonj1998@naver.com>
2022-02-07 23:43:19 +0900

    2022.02.07.

--- ch3/src/main/java/com/fastcampus/ch3/SpringDiTest.java
+++ ch3/src/main/java/com/fastcampus/ch3/SpringDiTest.java
@@ -4,23 +4,37 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
 
 @Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor // 기본 생성자 꼭 만들어 주자
+@Component
 class Car {
-    String color;
-    int oil;
-    Engine engine;
-    Door[] doors;
+    @Value("red") String color;
+    @Value("100") int oil;
+//    @Autowired
+//    @Qualifier("superEngine")
+    @Resource(name = "superEngine")
+    Engine engine; // byType
+    @Autowired Door[] doors;
 }
 
-class Engine {}
-class Door {}
+@Component("engine") class Engine {} //<bean id="engine" class="com.fastcampus.ch3.Engine"/>
+@Component class SuperEngine extends Engine{}
+@Component class TurboEngine extends Engine{}
+@Component class Door {}
 
+@Component
 public class SpringDiTest {
     public static void main(String[] args) {
         ApplicationContext ac = new GenericXmlApplicationContext("config.xml");
@@ -28,19 +42,22 @@ public class SpringDiTest {
         // ac.getBean("car").var == ctrl + alt + v
         // shift + tab : Class 변경 및 다운 캐스팅
         Car car = (Car) ac.getBean("car"); // byName
-        Car car2 = ac.getBean(Car.class); // byType
+//        Car car2 = ac.getBean(Car.class); // byType
 //        Car car3 = ac.getBean("car", Car.class);
 
-        Engine engine = (Engine) ac.getBean("engine");
+//        Engine engine = (Engine) ac.getBean("superEngine");
+//        Engine engine = ac.getBean(Engine.class);
+//        Engine engine = (Engine) ac.getBean("engine");
         Door door = (Door) ac.getBean("door");
 
 //        config.xml 에서 property 태그로 설정 해 줌(setter 필요)
-        car.setColor("red");
-        car.setOil(100);
-        car.setEngine(engine);
-        car.setDoors(new Door[]{ ac.getBean(Door.class), ac.getBean(Door.class) });
-
+//        car.setColor("red");
+//        car.setOil(100);
+//        car.setEngine(engine);
+//        car.setDoors(new Door[]{ ac.getBean(Door.class), ac.getBean(Door.class) });
+//        setter 대신 @Autowired, @Value 이용
+        
         System.out.println("car = " + car);
-        System.out.println("car2 = " + car2);
+//        System.out.println("engine = " + engine);
     }
 }
